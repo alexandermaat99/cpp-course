f7cdaf6da3166be226801f0e310910e0115688b8
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-03-20 21:24:42 -0600

    project one

--- Chapter02/Exercise/Exercise/AdFunctions.h
+++ Chapter02/Exercise/Exercise/AdFunctions.h
@@ -1,3 +1,13 @@
 #pragma once
 
 #include "AdTypes.h"
+
+namespace Ad
+{
+
+namespace Utils
+{
+float kptToMps(const float kph);
+}
+
+} // namespace Ad
--- Chapter02/Exercise/Exercise/AdTypes.h
+++ Chapter02/Exercise/Exercise/AdTypes.h
@@ -3,3 +3,26 @@
 #include <cstdint>
 
 #include "AdConstants.h"
+
+namespace Ad
+{
+namespace Types
+{
+enum class LaneAssociationType
+{
+    LEFT,
+    CENTER,
+    RIGHT,
+    NONE
+};
+
+struct VehicleType
+{
+    int id;
+    LaneAssociationType lane;
+    float speedMps;
+    float distanceMeters;
+};
+
+}; // namespace Types
+} // namespace Ad
